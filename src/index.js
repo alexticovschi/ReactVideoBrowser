45f980e01063b715644cc51930f7793851fbdbb9
AlexTicovschi <alexmticovschi@gmail.com>
2017-12-17 13:06:38 +0000

    install youtube-api-search

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// youtube API key
+const API_KEY = 'AIzaSyDfyiymG6qCLhbUeYTHJLdt527JFvGc5dU';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component. This component should produce some HTML
+const App = () => <div>Hi ho!</div>;
+
+
+// Take the component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector(".container"));
+
+
+
+
