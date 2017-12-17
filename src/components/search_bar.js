f0cfa11ee530d4ef8a397cde9d0867fbd6b67f77
AlexTicovschi <alexmticovschi@gmail.com>
2017-12-17 13:45:51 +0000

    create a class-based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // declare component named SearchBar that contains an input
-const SearchBar = () => {
-    return <input />;
-};
+class SearchBar extends Component {
+    render() {
+        return <input />;
+    }
+}
 
 // export the component
 export default SearchBar;
