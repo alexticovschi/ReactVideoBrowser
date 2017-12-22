e1783cf4ec041bd96f9d8f3a82c9c78d0235c499
AlexTicovschi <alexmticovschi@gmail.com>
2017-12-22 13:05:23 +0000

    build lists with map

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 
-// declare component named SearchBar that contains an input
+// create a class-based component named SearchBar that contains an input
 class SearchBar extends Component {
-    render() {
-        return <input />;
-    }
-}
+    constructor(props) {
+        super(props);
 
+        this.state = { term: '' };
+    }
+    render() {
+        // add an event handler to the input element
+        // the event handler will be triggered whenever the event occurs
+        // when the input changes, onInputChange will be executed;
+        return (
+            <div>
+                {/* whenever we update the input element, whenever we change the value of it 
+                    we change the state(this.state.term) of the element with the new value of the input 
+                    - whenever we update our state, whenever we call this.setState it causes our component to automatically rerender
+                      and push all the updated information into the DOM*/}
+                <input onChange={event => this.setState({ term: event.target.value })} />
+                <br/>
+                {this.state.term}
+            </div>
+            
+        );
+    }
+
+    
+}
 // export the component
 export default SearchBar;
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar'; // import the component from search_bar.js
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyDfyiymG6qCLhbUeYTHJLdt527JFvGc5dU'; // youtube API key
 
-// import the component from search_bar.js
-import SearchBar from './components/search_bar';
-
-// youtube API key
-const API_KEY = 'AIzaSyDfyiymG6qCLhbUeYTHJLdt527JFvGc5dU';
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'grey cats'}, (videos) => {
+            this.setState({ videos: videos });
+        });
+    }
+
+    //state = { videos: [] };
+
+    render() {  
+        // YTSearch({key: API_KEY, term: 'grey cats'}, (videos) => {
+        //     this.setState({ videos: videos });
+        // });
+
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 
